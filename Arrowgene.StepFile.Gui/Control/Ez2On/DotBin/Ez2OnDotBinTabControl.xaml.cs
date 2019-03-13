27a526aad950baef0a917273899231a5ebc3c6b9
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2019-03-13 10:25:34 +0800

    icons

---
+++ Arrowgene.StepFile.Gui/Control/Ez2On/DotBin/Ez2OnDotBinTabControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Arrowgene.StepFile.Control.Tab;
+using System.Windows.Input;
+
+namespace Arrowgene.StepFile.Control.Ez2On.DotBin
+{
+    public partial class Ez2OnDotBinTabControl : TabUserControl
+    {
+        private ICommand _openCommand;
+        private ICommand _saveCommand;
+
+        public ICommand OpenCommand { get { return _openCommand; } set { _openCommand = value; OnPropertyChanged("OpenCommand"); } }
+        public ICommand SaveCommand { get { return _saveCommand; } set { _saveCommand = value; OnPropertyChanged("SaveCommand"); } }
+
+        public Ez2OnDotBinTabControl()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
---
+++ Arrowgene.StepFile.Gui/Control/Ez2On/DotBin/Ez2OnDotBinTabController.cs
@@ -0,0 +1,37 @@
+﻿using Arrowgene.Services.Logging;
+using Arrowgene.StepFile.Control.Tab;
+using Arrowgene.StepFile.Core;
+using System.IO;
+
+namespace Arrowgene.StepFile.Control.Ez2On.DotBin
+{
+    public class Ez2OnDotBinTabController : TabController
+    {
+        private Ez2OnDotBinTabControl _ez2OndotBinTabControl;
+
+        public Ez2OnDotBinTabController() : base(new Ez2OnDotBinTabControl())
+        {
+            _ez2OndotBinTabControl = TabUserControl as Ez2OnDotBinTabControl;
+
+            Header = "Ez2On StepFile";
+
+            _ez2OndotBinTabControl.OpenCommand = new CommandHandler(Open, true);
+            _ez2OndotBinTabControl.SaveCommand = new CommandHandler(Save, true);
+        }
+        private void Open()
+        {
+            FileInfo selected = new SelectFileBuilder()
+                .Filter("Ez2On StepFile(*.ptn) | *.ptn")
+                .SelectSingle();
+            if (selected == null)
+            {
+                return;
+            }
+        }
+
+        private void Save()
+        {
+
+        }
+    }
+}
